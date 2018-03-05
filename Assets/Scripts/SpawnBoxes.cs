93ea7a0aaf4afbd8c08bf60891fe8a40844da7ea
Hannah Beilinson <hcbeilinson@gmail.com>
2018-03-05 17:46:09 -0500

    Populating boxes

---
+++ Assets/Scripts/SpawnBoxes.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBoxes : MonoBehaviour {
+
+	public Vector3 center;
+	public Vector3 size;
+
+
+	public GameObject Boxprefab;
+
+	// Use this for initialization
+	void Start () {
+
+		float rand = Random.Range (500, 1000);
+		for (int i = 0; i < rand; i++) {
+			SpawnBox ();
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.Q)) {
+			SpawnBox ();
+		}
+		
+	}
+
+	public void SpawnBox()
+	{
+		Vector3 pos = center + new Vector3 (Random.Range(-size.x/2, size.x/2), Random.Range(-size.y/2, size.y/2), Random.Range(-size.z/2, size.z/2));
+		Instantiate(Boxprefab, pos, Quaternion.identity);
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = new Color (1, 0, 0, 0.5f);
+		Gizmos.DrawCube (center, size);
+	}
+
+
+}
