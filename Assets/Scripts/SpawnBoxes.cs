5340b34331061e04438f59f3d67116fd9f9ed423
Hannah Beilinson <hcbeilinson@gmail.com>
2018-03-22 18:34:04 -0400

    Reticle working more

--- Assets/Scripts/SpawnBoxes.cs
+++ Assets/Scripts/SpawnBoxes.cs
@@ -14,7 +14,7 @@ public class SpawnBoxes : MonoBehaviour {
 	void Start () {
 
 		float rand = Random.Range (500, 500);
-		for (int i = 0; i < rand; i++) {
+		for (int i = 0; i < 50; i++) {
 			SpawnBox ();
 		}
 		
@@ -32,7 +32,9 @@ public class SpawnBoxes : MonoBehaviour {
 	public void SpawnBox()
 	{
 		Vector3 pos = center + new Vector3 (Random.Range(-size.x/2, size.x/2), Random.Range(-size.y/2, size.y/2), Random.Range(-size.z/2, size.z/2));
-		Instantiate(Boxprefab, pos, Quaternion.identity);
+		//if ((pos.x - 20) > 5 && (pos.y - 23) > 5 && (pos.z - 20) > 5 && (20 - pos.x) > 5 && (23- pos.y) >5 && (20 - pos.z) > 5) {
+			Instantiate (Boxprefab, pos, Quaternion.identity);
+		//}
 	}
 
 	void OnDrawGizmosSelected()
---
+++ Assets/Scripts/targetMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetMove : MonoBehaviour {
+
+	Vector3 newPosition;
+	void Start () {
+		//newPosition = transform.position;
+	}
+	void Update()
+	{
+		OnMouseOver ();
+		if (Input.touchCount>0)
+		{
+			print ("touched");
+			/*
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))
+			{
+				newPosition = hit.point;
+				transform.position = newPosition;
+			}
+			*/
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			//print ("clicked");
+
+		}
+	}
+	void OnMouseOver() {
+		if (Input.GetMouseButtonDown (0)) {
+			//print ("clicked on a box");
+
+		}
+	}
+
+	void OnPointerClick() {
+		print ("pointer clicked");
+	}
+}
